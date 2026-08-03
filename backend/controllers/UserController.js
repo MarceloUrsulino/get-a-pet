adf4b8f93e2364e76186f8d4b4ee6c3b8164a4b9
MarceloUrsulino <marceloursulino40@gmail.com>
2026-08-03 18:09:43 -0300

    Resgatando usuário por id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -136,5 +136,16 @@ module.exports = class UserController{
         // Responde com os dados do usuário logado (ou null, se não estava logado)
         res.status(200).send(currentUser)
     }
-    
+    static async getUserById(req, res) {
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            
+            res.status(422).json({message: 'Usuário não encontrado.'})
+            return
+        }
+        res.status(200).json({ user })
+    }
 }
